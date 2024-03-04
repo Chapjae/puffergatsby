b437f319af29e715403c32636a4fd78fee81ee6d
Jay Liu <jayliu89@gmail.com>
2024-03-03 17:20:27 -0800

    figured out how to import room images into booking.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,21 +4,29 @@
 module.exports = {
   siteMetadata: {
     title: `Puffer Isle Inn`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
     },
-    __key: "pages"
-  }]
+  ],
 };
--- src/pages/booking.js
+++ src/pages/booking.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { StaticImage, GatsbyImage } from 'gatsby-plugin-image';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const Booking = () => {
+  const data = useStaticQuery(graphql`
+    query RoomImages {
+      allFile(filter: { relativeDirectory: { eq: "rooms" } }) {
+        edges {
+          node {
+            childImageSharp {
+              gatsbyImageData(layout: FIXED)
+            }
+          }
+        }
+      }
+    }
+  `);
   return (
     <Layout>
-      <div></div>
+      <h1>Choose a Room</h1>
+      {data.allFile.edges.map(({ node }, index) => (
+        <GatsbyImage
+          key={index}
+          image={node.childImageSharp.gatsbyImageData}
+          alt={`Room ${index + 1}`}
+        />
+      ))}
     </Layout>
   );
 };
